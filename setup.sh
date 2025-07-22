352673409fac2760cd891aeeb3d8c69954cd75c8
christoph00 <christoph@asche.co>
2025-07-22 09:35:24 +0200

    ok

--- setup.sh
+++ setup.sh
@@ -38,7 +38,7 @@ rc_add core-setup default
 
 rc_add sshd default
 
-chmod 700 /etc/monitrc
+chmod 700 "$ROOTFS"/etc/monitrc
 
 # einfo "Set Root Password"
 # sed -i 's|root.*|root:$1$n3vjdweX$vyZqcZyUC5Q2uq4bxnfbQ0:18242:0:99999:7:::|g' "$ROOTFS"/etc/shadow
